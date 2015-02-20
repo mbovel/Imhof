797fe549f79a3569c1113ba04e26093879d63a6d
mbovel <matthieu@bovel.net>
2015-02-20 10:12:34 +0100

    Answer question about imports
    “In Java, import is simply used by the compiler to let you name your
    classes by their unqualified name, let's say String instead of
    java.lang.String.”

--- src/ch/epfl/imhof/projection/Projection.java
+++ src/ch/epfl/imhof/projection/Projection.java
@@ -1,16 +1,11 @@
 package ch.epfl.imhof.projection;
 
-//ça ne marche pas sans les imports mais comme je suis une burne en portée je sais pas trop pourquoi ...
-import ch.epfl.imhof.*;
-import ch.epfl.imhof.geometry.*;
+// How java import works
+// http://stackoverflow.com/a/12620773
+import ch.epfl.imhof.geometry.Point;
+import ch.epfl.imhof.PointGeo;
 
-/*
- * je sais pas si j'ai bien fait de mettre la classe en abstract ...
- * y a rien de précisé dans la donnée !
- * De plus je crois que c'est un interface
- * */
 public interface Projection {
-
     public Point project(PointGeo point);
 
     public PointGeo inverse(Point point);
