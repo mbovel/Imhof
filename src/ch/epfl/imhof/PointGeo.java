9b395ca28aa150ec2f1de83787579360530976d9
mbovel <matthieu@bovel.net>
2015-02-20 10:11:01 +0100

    Better exception handling

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -5,15 +5,24 @@ public final class PointGeo {
     private final double longitude;
     private final double latitude;
 
+    static private final double LONGITUDE_LOWER_BOUND = -java.lang.Math.PI;
+    static private final double LONGITUDE_UPPER_BOUND = java.lang.Math.PI;
+    static private final double LATITUDE_LOWER_BOUND = -java.lang.Math.PI / 2;
+    static private final double LATITUDE_UPPER_BOUND = java.lang.Math.PI / 2;
+
     public PointGeo(double longitude, double latitude) {
-
-        if (latitude < -(java.lang.Math.PI) / 2
-                || latitude > (java.lang.Math.PI) / 2) {
-            throw new IllegalArgumentException();
-        }
-
-        if (longitude < -(java.lang.Math.PI) || longitude > java.lang.Math.PI) {
-            throw new IllegalArgumentException();
+        if (longitude < LONGITUDE_LOWER_BOUND) {
+            throw new IllegalArgumentException("longitude should be at least "
+                    + LONGITUDE_LOWER_BOUND);
+        } else if (longitude > LONGITUDE_UPPER_BOUND) {
+            throw new IllegalArgumentException("longitude should be at most "
+                    + LONGITUDE_UPPER_BOUND);
+        } else if (latitude < LATITUDE_LOWER_BOUND) {
+            throw new IllegalArgumentException("longitude should be at least "
+                    + LATITUDE_LOWER_BOUND);
+        } else if (latitude > LATITUDE_UPPER_BOUND) {
+            throw new IllegalArgumentException("longitude should be at most "
+                    + LATITUDE_UPPER_BOUND);
         }
 
         this.latitude = latitude;
