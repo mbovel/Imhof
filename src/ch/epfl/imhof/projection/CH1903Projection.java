f736a0bb17c65d07d2327ef522e106981034cb27
mbovel <matthieu@bovel.net>
2015-02-20 12:23:37 +0100

    Correct indentation in unformatted sections

--- src/ch/epfl/imhof/projection/CH1903Projection.java
+++ src/ch/epfl/imhof/projection/CH1903Projection.java
@@ -15,15 +15,15 @@ public final class CH1903Projection implements Projection {
         // The longitude lambda and the latitude phi are in degrees
         // @formatter:off
         lambda_1 = (1 / 10000.0) * (lambda * 3600.0 - 26782.5);
-        
+
            phi_1 = (1 / 10000.0) * (phi * 3600.0 - 169028.66);
-        
+
                x =   600072.37
                    + 211455.93 * lambda_1
                    - 10938.51 * lambda_1 * phi_1
                    - 0.36 * lambda_1 * Math.pow(phi_1, 2.0)
                    - 44.54 * Math.pow(lambda_1, 3.0);
-        
+
                y =   200147.07
                    + 308807.95 * phi_1
                    + 3745.25 * Math.pow(lambda_1, 2.0)
@@ -40,24 +40,24 @@ public final class CH1903Projection implements Projection {
 
         // @formatter:off
              x_1 = (point.x() - 600000.0) / 1000000.0;
-       
+
              y_1 = (point.y() - 200000.0) / 1000000.0;
-        
+
         lambda_0 =   2.6779094
                    + 4.728982 * x_1
                    + 0.791484 * x_1 * y_1
                    + 0.1306 * x_1 * Math.pow(y_1, 2.0)
                    - 0.0436 * Math.pow(x_1, 3.0);
-        
+
            phi_0 =   16.9023892
                    + 3.238272 * y_1
                    - 0.270978 * Math.pow(x_1, 2.0)
                    - 0.002528 * Math.pow(y_1, 2.0)
                    - 0.0447 * Math.pow(x_1, 2.0) * y_1
                    - 0.0140 * Math.pow(y_1, 3.0);
-        
+
           lambda = lambda_0 * (100.0 / 36.0);
-        
+
              phi = phi_0 * (100.0 / 36.0);
         // @formatter:on
 
