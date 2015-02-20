ad977d2a32aad0dc405c686d14a15ccb391a100f
mbovel <matthieu@bovel.net>
2015-02-20 12:26:11 +0100

    Document Point

--- src/ch/epfl/imhof/geometry/Point.java
+++ src/ch/epfl/imhof/geometry/Point.java
@@ -1,19 +1,38 @@
 package ch.epfl.imhof.geometry;
 
+/**
+ * A point, represented by its cartesian coordinates.
+ * 
+ * @author Matthieu Bovel (250300)
+ * @author Matteo Besançon (245826)
+ */
 public final class Point {
-
     private final double x;
     private final double y;
 
+    /**
+     * Constructs a point, given its x-coordinate and y-coordinate
+     * 
+     * @param x
+     *            point's abscissa
+     * @param y
+     *            point's ordinate
+     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return point's abscissa
+     */
     public double x() {
         return x;
     }
 
+    /**
+     * @return point's ordinate
+     */
     public double y() {
         return y;
     }
