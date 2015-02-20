4625a15548cad93efc88ee663e01f0519d5054d2
mbovel <matthieu@bovel.net>
2015-02-20 12:17:28 +0100

    Make PointGeo doc

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -1,5 +1,12 @@
 package ch.epfl.imhof;
 
+/**
+ * A point on earth, represented by its spherical coordinates (longitude and
+ * latitude).
+ * 
+ * @author Matthieu Bovel (250300)
+ * @author Matteo Besançon (245826)
+ */
 public final class PointGeo {
 
     private final double longitude;
@@ -10,7 +17,24 @@ public final class PointGeo {
     static private final double LATITUDE_LOWER_BOUND = -java.lang.Math.PI / 2;
     static private final double LATITUDE_UPPER_BOUND = java.lang.Math.PI / 2;
 
-    public PointGeo(double longitude, double latitude) {
+    /**
+     * Construct a point, given its longitude and latitude
+     * 
+     * @param longitude
+     *            point's longitude, in radians
+     * @param latitude
+     *            point's latitude, in radians
+     * @throws IllegalArgumentException
+     *             if longitude is smaller than -PI
+     * @throws IllegalArgumentException
+     *             if longitude is bigger than PI
+     * @throws IllegalArgumentException
+     *             if latitude is smaller than -PI/2
+     * @throws IllegalArgumentException
+     *             if latitude is bigger than PI/2
+     */
+    public PointGeo(double longitude, double latitude)
+            throws IllegalArgumentException {
         if (longitude < LONGITUDE_LOWER_BOUND) {
             throw new IllegalArgumentException("longitude should be at least "
                     + LONGITUDE_LOWER_BOUND);
@@ -32,10 +56,16 @@ public final class PointGeo {
         this.longitude = longitude;
     }
 
+    /**
+     * @return point's longitude
+     */
     public double longitude() {
         return longitude;
     }
 
+    /**
+     * @return point's latitude
+     */
     public double latitude() {
         return latitude;
     }
