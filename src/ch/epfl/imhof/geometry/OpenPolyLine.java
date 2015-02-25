7e5c8c6d90342d56123341827c919e7338fb192d
matteo113 <besancon.matteo@gmail.com>
2015-02-25 22:02:39 +0100

    ecriture et commentaire de la class OpenPolyLine

--- src/ch/epfl/imhof/geometry/OpenPolyLine.java
+++ src/ch/epfl/imhof/geometry/OpenPolyLine.java
@@ -1,5 +1,26 @@
 package ch.epfl.imhof.geometry;
 
+import java.util.List;
+
+/**
+ * 
+ * @author matteo113
+ *
+ */
 public final class OpenPolyLine extends PolyLine {
 
+	/**
+	 * construct an open polyline by calling the constructor from the super class PolyLine
+	 * 
+	 *@param list of point of the polyline
+	 */
+	public OpenPolyLine(List<Point> points) {
+		super(points);
+	}
+	/**
+	 *@return return false because the Polyline isn't closed
+	 */
+	public boolean isClosed() {
+		return (false);
+	}
 }
