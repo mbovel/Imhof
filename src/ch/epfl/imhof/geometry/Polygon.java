908b668ca63273b4f17d0666b5fd1928052b2247
mbovel <matthieu@bovel.net>
2015-02-27 11:51:08 +0100

    Make class Polygon

--- src/ch/epfl/imhof/geometry/Polygon.java
+++ src/ch/epfl/imhof/geometry/Polygon.java
@@ -1,5 +1,31 @@
 package ch.epfl.imhof.geometry;
 
-public final class Polygon {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * @author Matthieu Bovel (250300)
+ *
+ */
+public final class Polygon {
+    private final ClosedPolyLine shell;
+    private final List<ClosedPolyLine> holes;
+
+    Polygon(ClosedPolyLine shell, List<ClosedPolyLine> holes) {
+        this.shell = shell;
+        this.holes = Collections.unmodifiableList(new ArrayList<ClosedPolyLine>(holes));
+    }
+
+    Polygon(ClosedPolyLine shell) {
+        this(shell, new ArrayList<ClosedPolyLine>());
+    }
+    
+    public ClosedPolyLine shell() {
+        return this.shell;
+    }
+    
+    public List<ClosedPolyLine> holes() {
+        return this.holes;
+    }
 }
