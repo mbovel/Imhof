b713f8f496aff4b0bd06127ac1b8fd52d0421d21
matteo113 <besancon.matteo@gmail.com>
2015-03-05 21:13:07 +0100

    writting the class OSMNode

--- src/ch/epfl/imhof/osm/OSMNode.java
+++ src/ch/epfl/imhof/osm/OSMNode.java
@@ -1,10 +1,39 @@
 package ch.epfl.imhof.osm;
 
+import ch.epfl.imhof.*;
+
 /**
  * @author Matteo Besançon (245826)
  *
+ *         Represents an OSM node with an identity (long), an object Attributes
+ *         which contains a map of attribute and a position (PointGeo).
  */
 public class OSMNode extends OSMEntity {
 
-	
+	private PointGeo position;
+
+	/**
+	 * Create a new osm node given its identification, attributes and position.
+	 * 
+	 * @param id
+	 *            identification of the osm node
+	 * @param position
+	 *            position of the osm node
+	 * @param attributes
+	 *            all the attributes of the osm node
+	 */
+	public OSMNode(long id, PointGeo position, Attributes attributes) {
+		super(id, attributes);
+		this.position = position;
+
+	}
+
+	/**
+	 * return the position of the node
+	 * 
+	 * @return the position of the node
+	 */
+	public PointGeo position() {
+		return position;
+	}
 }
