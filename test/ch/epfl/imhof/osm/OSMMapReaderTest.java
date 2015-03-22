e78aa99fc8a1b90325ff96fd317acc8e7e0631ce
mbovel <matthieu@bovel.net>
2015-03-22 20:07:34 +0100

    Add some OSMMapReader tests

--- test/ch/epfl/imhof/osm/OSMMapReaderTest.java
+++ test/ch/epfl/imhof/osm/OSMMapReaderTest.java
@@ -8,10 +8,58 @@ import org.junit.Test;
 import org.xml.sax.SAXException;
 
 public class OSMMapReaderTest {
+    private static final double DELTA = 0.000001;
+
+    private static void checkNode(OSMNode node, long id, double latDeg,
+            double lonDeg) {
+        assertEquals(node.id(), id);
+        assertEquals(node.position().latitude(), Math.toRadians(latDeg), DELTA);
+        assertEquals(node.position().longitude(), Math.toRadians(lonDeg), DELTA);
+    }
+
     @Test
     public void nodeIsCorrectlyParsed() throws IOException, SAXException {
-        OSMMap.Builder mapBuilder = OSMMapReader.readOSMFileToBuilder("test/ch/epfl/imhof/osm/data/node.xml", false);
+        OSMMap.Builder mapBuilder = OSMMapReader.readOSMFileToBuilder(
+                "test/ch/epfl/imhof/osm/data/node_ok.xml", false);
+        checkNode(mapBuilder.nodeForId(1), 1, 47.0, 7.0);
+    }
+
+    @Test(expected = OSMMissingAttributeException.class)
+    public void missingIDThrowsException() throws IOException, SAXException {
+        OSMMapReader.readOSMFileToBuilder(
+                "test/ch/epfl/imhof/osm/data/node_missing_id.xml", false);
+    }
+
+    // Could add missingLonThrowsException
+
+    // Could add missingLatThrowsException
+
+    @Test
+    public void wayIsCorrectlyParsed() throws IOException, SAXException {
+        OSMMap.Builder mapBuilder = OSMMapReader.readOSMFileToBuilder(
+                "test/ch/epfl/imhof/osm/data/way_ok.xml", false);
+
+        OSMWay way = mapBuilder.wayForId(3);
+
+        assertEquals(way.id(), 3);
+        assertEquals(way.attributeValue("highway"), "unclassified");
         
-        OSMNode node = mapBuilder.nodeForId(256186062);
+        checkNode(way.firstNode(), 1, 47.0, 7.0);
+        checkNode(way.lastNode(), 2, 47.0, 6.0);
+    }
+    
+    @Test
+    public void relationIsCorrectlyParsed() throws IOException, SAXException {
+        OSMMap.Builder mapBuilder = OSMMapReader.readOSMFileToBuilder(
+                "test/ch/epfl/imhof/osm/data/relation_ok.xml", false);
+
+        OSMRelation relation = mapBuilder.relationForId(11);
+
+        assertEquals(relation.id(), 11);
+        assertEquals(relation.attributeValue("type"), "multipolygon");
+        
+        OSMWay aWay = (OSMWay)relation.members().get(0).member();
+        
+        assertEquals(aWay.id(), 9);
     }
 }
