27d4bf2618f88aa2a587c7b32cb26565baa05646
mbovel <matthieu@bovel.net>
2015-03-26 23:39:35 +0100

    Add doc to OSMMissingAttributeException

--- src/ch/epfl/imhof/osm/OSMMissingAttributeException.java
+++ src/ch/epfl/imhof/osm/OSMMissingAttributeException.java
@@ -5,6 +5,9 @@ import org.xml.sax.SAXParseException;
 
 // How to define custom exception class in Java, the easiest way?
 // http://stackoverflow.com/questions/3776327/how-to-define-custom-exception-class-in-java-the-easiest-way
+/**
+ * @author Matthieu Bovel (250300)
+ */
 public class OSMMissingAttributeException extends SAXParseException {
     // What is a serialVersionUID and why should I use it?
     // http://stackoverflow.com/a/285809
