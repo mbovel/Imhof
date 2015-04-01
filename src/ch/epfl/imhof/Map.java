aff108486619b373c6fe93132ae565f9bd2590dc
matteo113 <besancon.matteo@gmail.com>
2015-04-01 18:05:23 +0200

    Add javadoc to Map

--- src/ch/epfl/imhof/Map.java
+++ src/ch/epfl/imhof/Map.java
@@ -1,4 +1,3 @@
-
 package ch.epfl.imhof;
 
 import java.util.ArrayList;
@@ -9,41 +8,83 @@ import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.geometry.Polygon;
 
 /**
- * 
+ * Represents a Map (nothing to do with the java interface Map) with geometric
+ * attributed identities.
  * 
  * @author Matthieu Bovel (250300)
  */
 public final class Map {
     private final List<Attributed<PolyLine>> polyLines;
-    private final List<Attributed<Polygon>> polygons;
-
+    private final List<Attributed<Polygon>>  polygons;
+    
+    /**
+     * Constructs a new <code>Map</code> given its Polylines and Polygons.
+     * 
+     * @param polyLines
+     *            <code>Map</code>'s {@link List} of {@link PolyLine}.
+     * @param polygons
+     *            <code>Map</code>'s {@link List} of {@link Polygon}.
+     */
     public Map(List<Attributed<PolyLine>> polyLines,
             List<Attributed<Polygon>> polygons) {
-        this.polyLines = Collections
-                .unmodifiableList(new ArrayList<>(polyLines));
+        this.polyLines = Collections.unmodifiableList(new ArrayList<>(polyLines));
         this.polygons = Collections.unmodifiableList(new ArrayList<>(polygons));
     }
-
+    
+    /**
+     * Returns all the <code>PolyLines</code> of the <code>Map</code>.
+     * 
+     * @return all the <code>PolyLines</code> of the <code>Map</code>.
+     */
     public List<Attributed<PolyLine>> polyLines() {
         return polyLines;
     }
-
+    
+    /**
+     * Returns all the <code>Polygons</code> of the <code>Map</code>.
+     * 
+     * @return all the <code>Polygons</code> of the <code>Map</code>.
+     */
     public List<Attributed<Polygon>> polygons() {
         return polygons;
     }
-
+    
+    /**
+     * A class that helps in the consctruction of a {@link Map}.
+     * 
+     * @author Matthieu Bovel (250300)
+     *
+     */
     static public class Builder {
         private List<Attributed<PolyLine>> polyLines = new ArrayList<>();
-        private List<Attributed<Polygon>> polygons = new ArrayList<>();
-
+        private List<Attributed<Polygon>>  polygons  = new ArrayList<>();
+        
+        /**
+         * Adds a new <code>PolyLine</code> to the futur <code>Map</code>.
+         * 
+         * @param polyLine
+         *            the futur <code>Map</code>'s <code>PolyLine</code>.
+         */
         public void addPolyLine(Attributed<PolyLine> polyLine) {
             this.polyLines.add(polyLine);
         }
-
+        
+        /**
+         * Adds a new <code>Polygon</code> to the futur <code>Map</code>.
+         * 
+         * @param polygon
+         *            the futur <code>Map</code>'s <code>Polygon</code>.
+         */
         public void addPolygon(Attributed<Polygon> polygon) {
             this.polygons.add(polygon);
         }
-
+        
+        /**
+         * Constructs a new <code>Map</code> instance using the datas provided
+         * by the <code>Map.Builder</code>.
+         * 
+         * @return the new <code>Map</code>.
+         */
         public Map build() {
             return new Map(polyLines, polygons);
         }
