742c419dd430f1e6470958d74d28731000eb943c
matteo113 <besancon.matteo@gmail.com>
2015-04-29 00:16:56 +0200

    Add OurRoadPainterGeneratorTest.java

---
+++ test/ch/epfl/imhof/painting/OurRoadPainterGeneratorTest.java
@@ -0,0 +1,43 @@
+package ch.epfl.imhof.painting;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import ch.epfl.imhof.Map;
+import ch.epfl.imhof.geometry.Point;
+import ch.epfl.imhof.osm.OSMMap;
+import ch.epfl.imhof.osm.OSMToGeoTransformer;
+import ch.epfl.imhof.osm.OurOSMMapReaderTest;
+import ch.epfl.imhof.projection.CH1903Projection;
+import ch.epfl.imhof.projection.Projection;
+
+public class OurRoadPainterGeneratorTest {
+    public static Map readMap(String path) throws IOException, SAXException {
+        OSMMap map = OurOSMMapReaderTest.readOSMFile(path);
+        Projection proj = new CH1903Projection();
+        return new OSMToGeoTransformer(proj).transform(map);
+    }
+    
+    @Test
+    public void worksWithExample() throws IOException, SAXException {
+        
+        
+        Map map = readMap("test/data/big/lausanne.osm.gz");
+        
+        // La toile
+        Point bl = new Point(532510, 150590);
+        Point tr = new Point(539570, 155260);
+        
+        Java2DCanvas canvas = new Java2DCanvas(bl, tr, 1600, 1060, 150,
+                Color.WHITE);
+        
+        // Dessin de la carte et stockage dans un fichier
+        SwissPainter.painter().drawMap(map, canvas);
+        
+        ImageIO.write(canvas.image(), "png", new File("loztot.png"));
+    }
+}
