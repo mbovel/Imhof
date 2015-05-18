ff9830dea67a3a8520b21df8036c4f15db1f721f
mbovel <matthieu@bovel.net>
2015-05-18 14:31:50 +0200

    Move OurHGTDigitalElevationModelTes to correct package

--- test/ch/epfl/imhof/dem/OurHGTDigitalElevationModelTest.java
+++ test/ch/epfl/imhof/dem/OurHGTDigitalElevationModelTest.java
@@ -1,4 +1,4 @@
-package ch.epfl.imhof.painting;
+package ch.epfl.imhof.dem;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -9,7 +9,6 @@ import javax.imageio.ImageIO;
 import org.junit.Test;
 
 import ch.epfl.imhof.PointGeo;
-import ch.epfl.imhof.dem.HGTDigitalElevationModel;
 import ch.epfl.imhof.geometry.Vector3d;
 
 public class OurHGTDigitalElevationModelTest {
@@ -20,8 +19,8 @@ public class OurHGTDigitalElevationModelTest {
         double w = 7.2;
         double e = 7.8;
         
-        double realWidth = n - s;
-        double realHeight = e - w;
+        double realHeight = n - s;
+        double realWidth = e - w;
         int imgWidth = 800;
         int imgHeight = 800;
         int[] out = new int[imgWidth * imgHeight];
@@ -29,18 +28,23 @@ public class OurHGTDigitalElevationModelTest {
         File demFile = new File("test/data/big/N46E007.hgt");
         HGTDigitalElevationModel dem = new HGTDigitalElevationModel(demFile);
         
-        BufferedImage imageOut = new BufferedImage(imgWidth, imgHeight,
-                BufferedImage.TYPE_INT_RGB);
+        BufferedImage imageOut = new BufferedImage(
+            imgWidth,
+            imgHeight,
+            BufferedImage.TYPE_INT_RGB);
         
         for (int i = 0; i != imgHeight; ++i) {
-            double lat = n - ((double) i / imgHeight) * realHeight;
+            double lat = Math
+                .toRadians(n - (double) i / imgHeight * realHeight);
+            
             for (int j = 0; j != imgWidth; ++j) {
-                double lon = w + ((double) j / imgWidth) * realWidth;
-                PointGeo point = new PointGeo(Math.toRadians(lon),
-                        Math.toRadians(lat));
+                double lon = Math.toRadians(w + (double) j / imgWidth
+                        * realWidth);
+                
+                PointGeo point = new PointGeo(lon, lat);
                 Vector3d normal = dem.normalAt(point).normalized();
-                float grey = (float) (1 - 0.5 * (normal.x() + 1));
-                // float grey = (float) (0.5 * (normal.y() + 1));
+                
+                float grey = (float) (0.5 * (normal.y() + 1));
                 Color color = new Color(grey, grey, grey);
                 out[i * imgWidth + j] = color.getRGB();
             }
