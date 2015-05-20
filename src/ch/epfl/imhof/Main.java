9738cf4986fcd3c6ee3ecaf223c7f05fe2eab61d
matteo113 <besancon.matteo@gmail.com>
2015-05-20 16:17:49 +0200

    Convert longitudes and latitude in Radians in Main

--- src/ch/epfl/imhof/Main.java
+++ src/ch/epfl/imhof/Main.java
@@ -26,10 +26,10 @@ public class Main {
     public static void main(String[] args) throws IOException, SAXException {
         String osmFile = args[0];
         String hgtFile = args[1];
-        PointGeo bl = new PointGeo(Double.parseDouble(args[2]),
-                Double.parseDouble(args[3]));
-        PointGeo tr = new PointGeo(Double.parseDouble(args[4]),
-                Double.parseDouble(args[5]));
+        PointGeo bl = new PointGeo(Math.toRadians(Double.parseDouble(args[2])),
+            Math.toRadians(Double.parseDouble(args[3])));
+        PointGeo tr = new PointGeo(Math.toRadians(Double.parseDouble(args[4])),
+            Math.toRadians(Double.parseDouble(args[5])));
         int res = Integer.parseInt(args[6]);
         String outputFile = args[7];
         
