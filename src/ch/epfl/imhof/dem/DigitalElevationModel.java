ae62e7af8c1e4ff3a36b5b898ff816b395fa1182
matteo113 <besancon.matteo@gmail.com>
2015-05-22 11:01:42 +0200

    Comment DigitalElevationModel

--- src/ch/epfl/imhof/dem/DigitalElevationModel.java
+++ src/ch/epfl/imhof/dem/DigitalElevationModel.java
@@ -4,8 +4,21 @@ import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.geometry.Vector3d;
 
 /**
+ * Represents a digital elevation model.
+ * 
  * @author Matthieu Bovel (250300)
  */
 public interface DigitalElevationModel extends AutoCloseable {
+    /**
+     * Returns a normal vector of a given point of the elevation model.
+     * 
+     * @param point
+     *            of the normal vector wanted
+     * 
+     * @return a normal vector of a given point of the elevation model
+     * 
+     * @throws IllegalArgumentException
+     *             if the point is not contained in the digital elevation model
+     */
     Vector3d normalAt(PointGeo point) throws IllegalArgumentException;
 }
